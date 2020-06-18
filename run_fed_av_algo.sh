fd6f7004f503dcdb5b4e78bbc24d98f3ec67313b
vermasrijan <vermasrijan44@gmail.com>
2020-06-18 16:56:54 +0530

    updated

--- run_fed_av_algo.sh
+++ run_fed_av_algo.sh
@@ -1,1 +1,1 @@
-python fed_av_algo.py --global_model_path ./coordinator/g1/ --test_dataset_path coordinator/g1/ --client_dir ./clients --metadata_path coordinator/g1/global_mod_metadata.json
+python fed_av_algo.py --global_model_path ./coordinator/g1/ --test_dataset_path coordinator/g1/ --client_dir ./clients --metadata_path coordinator/g1
