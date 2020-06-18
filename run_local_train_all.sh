7416f046456a89ac7e2fdc5d1e778ab525bda215
vermasrijan <vermasrijan44@gmail.com>
2020-06-18 15:10:38 +0530

    cleared git cache

---
+++ run_local_train_all.sh
@@ -0,0 +1,5 @@
+python local_train.py --global_model_path ./coordinator/g1/ --local_model_path ./clients/c1/l1/ --local_dataset_path ./clients/c1/l1/train_data.pkl --metadata_path ./clients/c1/l1/metadata.json
+python local_train.py --global_model_path ./coordinator/g1/ --local_model_path ./clients/c2/l1/ --local_dataset_path ./clients/c2/l1/train_data.pkl --metadata_path ./clients/c2/l1/metadata.json
+python local_train.py --global_model_path ./coordinator/g1/ --local_model_path ./clients/c3/l1/ --local_dataset_path ./clients/c3/l1/train_data.pkl --metadata_path ./clients/c3/l1/metadata.json
+python local_train.py --global_model_path ./coordinator/g1/ --local_model_path ./clients/c4/l1/ --local_dataset_path ./clients/c4/l1/train_data.pkl --metadata_path ./clients/c4/l1/metadata.json
+python local_train.py --global_model_path ./coordinator/g1/ --local_model_path ./clients/c5/l1/ --local_dataset_path ./clients/c5/l1/train_data.pkl --metadata_path ./clients/c5/l1/metadata.json
