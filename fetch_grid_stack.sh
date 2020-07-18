24a76e875a1d1959440e1b818fd420bd023ce267
vermasrijan <vermasrijan44@gmail.com>
2020-07-18 10:58:14 +0530

    Added submodules

--- fetch_grid_stack.sh
+++
@@ -1,4 +0,0 @@
-# Cloning additional dependencies
-git clone https://github.com/OpenMined/PyGrid.git
-git clone https://github.com/OpenMined/PyGridNode.git
-git clone https://github.com/OpenMined/PyGridNetwork.git
