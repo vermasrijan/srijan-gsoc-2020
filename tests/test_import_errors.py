d62bf6a586578302032f24b513b10558da3e8177
vermasrijan <vermasrijan44@gmail.com>
2020-08-30 03:00:00 +0530

    adding travis tests

---
+++ tests/test_import_errors.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pyarrow.parquet as pq
+from typing import NamedTuple
+import random
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+import syft as sy
+import torch
+
+print("No Import Errors")
